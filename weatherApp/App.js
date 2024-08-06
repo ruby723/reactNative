79f19f7b3b995495728d32e5da89f00dc65a63ae
ruby723 <molly723@naver.com>
2024-08-06 14:43:01 +0900

    react native

--- weatherApp/App.js
+++ weatherApp/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>헤헤 어플 시작?????
+        왜 안바껴 이 미친것들아 
+        바로바로 바뀌라고</Text>
       <StatusBar style="auto" />
     </View>
   );
